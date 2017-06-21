7a866c5d8f4a031d305a1123e8af75107c3b2d55
romamaxime <roma.maxime1994@gmail.com>
2017-06-21 16:27:14 +0200

    add array ex1

---
+++ array/ex1.php
@@ -0,0 +1,8 @@
+<?php
+  $mois = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+
+  echo '<pre>';
+  print_r($mois);
+  echo '</pre>';
+
+?>
