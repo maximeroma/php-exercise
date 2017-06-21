bd7e5168e2ed5088148f03ca1ac1ab33ede676e8
romamaxime <roma.maxime1994@gmail.com>
2017-06-21 17:13:52 +0200

    add ex7

---
+++ array/ex7.php
@@ -0,0 +1,14 @@
+<?php
+  $département = array(
+    "62" => "Pas de Calais",
+    "59" => "Nord",
+    "80" => "Somme",
+    "02" => "Aisne",
+    "60" => "Oise",
+  );
+  $département["51"] = "Marne";
+
+  echo '<pre>';
+  print_r($département);
+  echo '</pre>'
+?>
