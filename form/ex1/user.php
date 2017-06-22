e6f296ec883ac6516d31f69f28a304192125ee2d
romamaxime <roma.maxime1994@gmail.com>
2017-06-22 13:44:54 +0200

    add ex3 & ex4

--- form/ex1/user.php
+++ form/ex1/user.php
@@ -1,5 +1,3 @@
 <?php
-  if (isset($_GET['nom']) AND isset($_GET['prenom'])){
-    echo $_GET['nom'] . ' ' . $_GET['prenom'];
-  }
+  echo 'success';
 ?>
--- form/ex2/user.php
+++ form/ex2/user.php
@@ -1,5 +1,3 @@
 <?php
-  if (isset($_POST['nom']) AND isset($_POST['prenom'])){
-    echo $_POST['nom'] . ' ' . $_POST['prenom'];
-  }
+  echo 'success';
 ?>
