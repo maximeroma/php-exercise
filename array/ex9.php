55144bfd8b9325d1d5413724a6de5c6cfdd8cb3c
romamaxime <roma.maxime1994@gmail.com>
2017-06-22 09:54:11 +0200

    add ex9

---
+++ array/ex9.php
@@ -0,0 +1,13 @@
+<?php
+  $département = array(
+    '62' => 'Pas de Calais',
+    '59' => 'Nord',
+    '80' => 'Somme',
+    '02' => 'Aisne',
+    '60' => 'Oise',
+  );
+
+  echo '<pre>';
+  print_r($département);
+  echo '</pre>';
+?>
